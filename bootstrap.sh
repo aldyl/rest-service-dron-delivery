e8f0a39dfc14e981e84961bd03b07ee07dac19e5
Ing. Aldy Leon Garcia <aldyleongarcia@gmail.com>
2022-10-19 13:33:41 -0400

    [UPD] Update usage bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-export FLASK_APP=./dispatch_controller/index.py
+[ "$1" == "app" ] &&export FLASK_APP=./dispatch_controller/app.py
+[ "$1" == "load_bd" ] && export FLASK_APP=./dispatch_controller/load_db
+[ "$1" == "test" ] && export FLASK_APP=./dispatch_controller/test.py
 
 pipenv run flask --debug run -h 0.0.0.0
