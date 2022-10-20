db7d730d278f3f26f9eae526b6d95c6e01b75b0e
Ing. Aldy Leon Garcia <aldyleongarcia@gmail.com>
2022-10-20 10:37:10 -0400

    [ADD] Readme configuration and test

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rest-service-dron-delivery
-conquest
+Install
+
+pip3 install pipenv
+pipenv  install --skip-lock  flask Flask-SQLAlchemy flask-marshmallow marshmallow-sqlalchemy marshmallow
+
+Load init database
+
+./bootstrap.sh load
+
+Execute main API
+
+./bootstrap.sh app
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 [ "$1" == "app" ] &&export FLASK_APP=./dispatch_controller/app.py
 [ "$1" == "load_bd" ] && export FLASK_APP=./dispatch_controller/load_db
-[ "$1" == "test" ] && export FLASK_APP=./dispatch_controller/test.py
+#[ "$1" == "test" ] && export FLASK_APP=./dispatch_controller/test.py
 
 pipenv run flask --debug run -h 0.0.0.0
